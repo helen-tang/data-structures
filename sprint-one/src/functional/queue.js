eef288c89cd2b9a468c5de4861f45634230e18c3
MKS-Student <mks.student@hackreactor.com>
2017-03-22 15:14:50 -0700

    Firgured out how to readjust indices, failed previous test in the process

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    var toBeDeleted = storage[0];
+    delete storage[0];
+    var storageArray = Object.keys(storage);
+    for (var i = 0; i < storageArray.length; i++) {
+      storage[i] = storage[i + 1];
+    }
+    index--;
+    return toBeDeleted;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
