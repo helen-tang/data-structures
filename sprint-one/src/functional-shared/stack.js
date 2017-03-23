e5a26457ac4a11b07a6891ce918ddcacc87a2255
MKS-Student <mks.student@hackreactor.com>
2017-03-22 20:03:48 -0700

    WIP functional-shared stack structure

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+
+  extend(someInstance, stackMethods);
+  return someInstance;
+};
+
+var extend = function(instance, methods) {
+    for (var key in methods) {
+        instance[key] = methods[key];
+    }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function() {
+
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+    console.log( this);
+    return Object.keys(this).length;
+};
 
