6890888f1bd53e9d5801fd3236fa0ad411867a0e
MKS-Student <mks.student@hackreactor.com>
2017-03-23 15:47:42 -0700

    Implemented Prototypal Stack - all tests pass

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.index = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
+stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+stackMethods.pop = function() {
+  var popped = this.storage[this.index - 1];
+  this.index--;
+  return popped;
+};
+
+stackMethods.size = function() {
+  if (this.index < 0) {
+    return 0;
+  }
+  return this.index; 
+};
