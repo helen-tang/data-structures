105e01481c019361b82cb7787326663ea5f78346
MKS-Student <mks.student@hackreactor.com>
2017-03-23 15:53:41 -0700

    Implemented prototypal queue - passed all tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.head = 0;
+  someInstance.tail = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.head] = value;
+  this.head ++;
+};
 
+queueMethods.dequeue = function () {
+  var toBeDequeued = this.storage[this.tail];
+  this.tail++;
+  if (this.tail > this.head) {
+    this.tail = this.head;
+  }
+  return toBeDequeued;
+};
+
+queueMethods.size = function () {
+  return this.head - this.tail;
+};
